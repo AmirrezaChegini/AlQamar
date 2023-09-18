65cb0b7f4a2405e19aa957321051c0a7d0979e1a
AmirrezaChegini <amirreza.chegini.1380@gmail.com>
2023-09-18 14:37:21 +0330

    feat: add theme & colors

---
+++ lib/constants/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+  static const Color blue = Color(0xff3F51B5);
+  static const Color grey = Color(0xff757575);
+}

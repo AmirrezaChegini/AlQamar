cc993e92d660a24006cef12afb81779aa438c5e1
AmirrezaChegini <amirreza.chegini.1380@gmail.com>
2023-09-18 18:22:48 +0330

    feat: header auth style

---
+++ lib/pages/main_wrapper/widgets/main_appbar.dart
@@ -0,0 +1,69 @@
+import 'package:al_qamar/constants/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class MainAppbar extends StatelessWidget implements PreferredSizeWidget {
+  const MainAppbar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: AppColors.transparent,
+      elevation: 0,
+      leading: Padding(
+        padding: const EdgeInsets.only(top: 6),
+        child: SvgPicture.asset(
+          'assets/icons/AlQamar_Logo.svg',
+          color: AppColors.red,
+        ),
+      ),
+      actions: [
+        Container(
+          alignment: Alignment.bottomCenter,
+          padding: const EdgeInsets.only(bottom: 10),
+          child: const Text(
+            'البت المباشر',
+            style: TextStyle(
+              color: AppColors.red,
+              fontSize: 10,
+            ),
+          ),
+        ),
+        const SizedBox(width: 4),
+        Image.asset(
+          'assets/icons/Live.png',
+          height: 30,
+          width: 30,
+          color: AppColors.red,
+        ),
+        const SizedBox(width: 4),
+        Image.asset(
+          'assets/icons/Menu.png',
+          height: 30,
+          width: 30,
+          color: AppColors.red,
+        ),
+        const SizedBox(width: 10),
+      ],
+      bottom: const PreferredSize(
+        preferredSize: Size.fromHeight(5),
+        child: Divider(
+          color: AppColors.red,
+          thickness: 2,
+          indent: 10,
+          endIndent: 10,
+        ),
+      ),
+      systemOverlayStyle: const SystemUiOverlayStyle(
+        statusBarColor: AppColors.transparent,
+        statusBarIconBrightness: Brightness.dark,
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(56);
+}

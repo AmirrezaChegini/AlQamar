cec375ca66a71661b06171e4617c24a8aa22a1b0
AmirrezaChegini <amirreza.chegini.1380@gmail.com>
2023-09-19 10:16:22 +0330

    fix: change image name

--- lib/constants/images.dart
+++ lib/constants/images.dart
@@ -3,10 +3,10 @@ class AppImages {
 
   static const String root = 'assets/images';
 
-  static const String scaffoldBackground = '$root/Gradient.png';
-  static const String splashBackground = '$root/Splash.png';
-  static const String authBackground = '$root/Auth_Background.png';
-  static const String authColorBackground = '$root/Auth_Color_Background.png';
+  static const String scaffoldBackground = '$root/gradient.png';
+  static const String splashBackground = '$root/splash.png';
+  static const String authBackground = '$root/auth_background.png';
+  static const String authColorBackground = '$root/auth_color_background.png';
   static const String img1 = '$root/img01.jpg';
   static const String img2 = '$root/img02.jpg';
   static const String img3 = '$root/img03.jpg';

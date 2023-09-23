7b2594f4832662565b26628a2efb6bcf7ab32cf7
AmirrezaChegini <amirreza.chegini.1380@gmail.com>
2023-09-23 12:26:50 +0330

    feat: add search page

---
+++ lib/pages/search/search_page.dart
@@ -0,0 +1,76 @@
+import 'package:al_qamar/constants/colors.dart';
+import 'package:al_qamar/pages/search/widgets/textfield_search.dart';
+import 'package:al_qamar/widgets/article_widget.dart';
+import 'package:flutter/material.dart';
+
+class SearchPage extends StatelessWidget {
+  const SearchPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 10),
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      decoration: BoxDecoration(
+        color: AppColors.white,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Column(
+        children: [
+          const SizedBox(height: 15),
+          Container(
+            height: 2,
+            width: MediaQuery.of(context).size.width / 3,
+            decoration: const BoxDecoration(
+              color: AppColors.grey600,
+            ),
+          ),
+          const SizedBox(height: 15),
+          const TextfielfSearch(),
+          const SizedBox(height: 10),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              const Expanded(
+                flex: 1,
+                child: Divider(),
+              ),
+              Text.rich(
+                TextSpan(
+                    text: 'حوالی 1202 نتیجه',
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleMedium!
+                        .copyWith(fontSize: 12),
+                    children: [
+                      TextSpan(
+                        text: ' "النص المطلوب"',
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyMedium!
+                            .copyWith(fontSize: 12),
+                      ),
+                    ]),
+              ),
+              const Expanded(
+                flex: 9,
+                child: Divider(),
+              ),
+            ],
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 6,
+              itemBuilder: (context, index) => const Padding(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                child: ArticleWidget(
+                  backgroundColor: AppColors.grey200,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}

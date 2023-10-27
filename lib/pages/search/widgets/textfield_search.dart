d36845e2df1abf80fd14b8ece324a771eda9a72e
Amirreza Chegini <amirreza.chegini.1380@gmail.com>
2023-10-27 19:36:59 +0330

    feat: rxdart feature

--- lib/pages/search/widgets/textfield_search.dart
+++ lib/pages/search/widgets/textfield_search.dart
@@ -6,8 +6,9 @@ import 'package:al_qamar/widgets/app_icon.dart';
 import 'package:al_qamar/widgets/icon_btn.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rxdart/rxdart.dart';
 
-class TextfielfSearch extends StatelessWidget {
+class TextfielfSearch extends StatefulWidget {
   const TextfielfSearch({
     super.key,
     required this.edtCtrl,
@@ -16,13 +17,32 @@ class TextfielfSearch extends StatelessWidget {
   final TextEditingController edtCtrl;
 
   @override
+  State<TextfielfSearch> createState() => _TextfielfSearchState();
+}
+
+class _TextfielfSearchState extends State<TextfielfSearch> {
+  final searchText = BehaviorSubject<String>();
+
+  @override
+  void initState() {
+    super.initState();
+    final debouncedSearch =
+        searchText.debounceTime(const Duration(seconds: 1)).distinct();
+
+    debouncedSearch.listen((event) {
+      BlocProvider.of<SearchBloc>(context).add(SearchArticleEvent(event));
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return TextField(
-      controller: edtCtrl,
+      controller: widget.edtCtrl,
       keyboardType: TextInputType.text,
       style: Theme.of(context).textTheme.bodyMedium!.copyWith(fontSize: 14),
-      onSubmitted: (value) => BlocProvider.of<SearchBloc>(context)
-          .add(SearchArticleEvent(edtCtrl.text)),
+      onChanged: (value) {
+        searchText.add(value);
+      },
       decoration: InputDecoration(
         enabledBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(10),
@@ -38,7 +58,7 @@ class TextfielfSearch extends StatelessWidget {
         ),
         contentPadding: const EdgeInsets.symmetric(horizontal: 20),
         suffixIcon: IconBtn(
-          onTap: () => edtCtrl.clear(),
+          onTap: () => widget.edtCtrl.clear(),
           child: const AppIcon(
             icon: AppIcons.close,
             width: 20,
